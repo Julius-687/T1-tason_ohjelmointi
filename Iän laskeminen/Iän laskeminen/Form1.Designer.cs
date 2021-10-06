428d85942333d6cc2c0d66de9eef92ad6e1b0eaf
Julius-687 <julius.ikavalko@gmail.com>
2021-10-06 16:03:19 +0300

    Iän laskeminen

--- Iän laskeminen/Iän laskeminen/Form1.Designer.cs
+++ Iän laskeminen/Iän laskeminen/Form1.Designer.cs
@@ -30,101 +30,108 @@
         {
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.VuosinaLB = new System.Windows.Forms.Label();
+            this.KuukausinaLB = new System.Windows.Forms.Label();
+            this.PaivinaLB = new System.Windows.Forms.Label();
+            this.TunteinaLB = new System.Windows.Forms.Label();
+            this.MinuutteinaLB = new System.Windows.Forms.Label();
+            this.SekunteinaLB = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // dateTimePicker1
             // 
             this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dateTimePicker1.Location = new System.Drawing.Point(12, 43);
             this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(204, 20);
+            this.dateTimePicker1.Size = new System.Drawing.Size(380, 35);
             this.dateTimePicker1.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button1.Location = new System.Drawing.Point(238, 40);
+            this.button1.Location = new System.Drawing.Point(422, 40);
             this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(77, 29);
+            this.button1.Size = new System.Drawing.Size(103, 45);
             this.button1.TabIndex = 1;
             this.button1.Text = "Laske";
             this.button1.UseVisualStyleBackColor = false;
             // 
-            // label1
+            // VuosinaLB
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 90);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.VuosinaLB.AutoSize = true;
+            this.VuosinaLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VuosinaLB.Location = new System.Drawing.Point(12, 96);
+            this.VuosinaLB.Name = "VuosinaLB";
+            this.VuosinaLB.Size = new System.Drawing.Size(99, 29);
+            this.VuosinaLB.TabIndex = 2;
+            this.VuosinaLB.Text = "Vuosina";
             // 
-            // label2
+            // KuukausinaLB
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 129);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.KuukausinaLB.AutoSize = true;
+            this.KuukausinaLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KuukausinaLB.Location = new System.Drawing.Point(12, 153);
+            this.KuukausinaLB.Name = "KuukausinaLB";
+            this.KuukausinaLB.Size = new System.Drawing.Size(137, 29);
+            this.KuukausinaLB.TabIndex = 3;
+            this.KuukausinaLB.Text = "Kuukausina";
             // 
-            // label3
+            // PaivinaLB
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 169);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "label3";
+            this.PaivinaLB.AutoSize = true;
+            this.PaivinaLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PaivinaLB.Location = new System.Drawing.Point(12, 214);
+            this.PaivinaLB.Name = "PaivinaLB";
+            this.PaivinaLB.Size = new System.Drawing.Size(91, 29);
+            this.PaivinaLB.TabIndex = 4;
+            this.PaivinaLB.Text = "Päivinä";
             // 
-            // label4
+            // TunteinaLB
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(77, 90);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "label4";
+            this.TunteinaLB.AutoSize = true;
+            this.TunteinaLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TunteinaLB.Location = new System.Drawing.Point(176, 96);
+            this.TunteinaLB.Name = "TunteinaLB";
+            this.TunteinaLB.Size = new System.Drawing.Size(107, 29);
+            this.TunteinaLB.TabIndex = 5;
+            this.TunteinaLB.Text = "Tunteina";
             // 
-            // label5
+            // MinuutteinaLB
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(77, 129);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
-            this.label5.TabIndex = 6;
-            this.label5.Text = "label5";
+            this.MinuutteinaLB.AutoSize = true;
+            this.MinuutteinaLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinuutteinaLB.Location = new System.Drawing.Point(176, 153);
+            this.MinuutteinaLB.Name = "MinuutteinaLB";
+            this.MinuutteinaLB.Size = new System.Drawing.Size(136, 29);
+            this.MinuutteinaLB.TabIndex = 6;
+            this.MinuutteinaLB.Text = "Minuutteina";
             // 
-            // label6
+            // SekunteinaLB
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(77, 169);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
-            this.label6.TabIndex = 7;
-            this.label6.Text = "label6";
+            this.SekunteinaLB.AutoSize = true;
+            this.SekunteinaLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SekunteinaLB.Location = new System.Drawing.Point(176, 214);
+            this.SekunteinaLB.Name = "SekunteinaLB";
+            this.SekunteinaLB.Size = new System.Drawing.Size(133, 29);
+            this.SekunteinaLB.TabIndex = 7;
+            this.SekunteinaLB.Text = "Sekunteina";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SekunteinaLB);
+            this.Controls.Add(this.MinuutteinaLB);
+            this.Controls.Add(this.TunteinaLB);
+            this.Controls.Add(this.PaivinaLB);
+            this.Controls.Add(this.KuukausinaLB);
+            this.Controls.Add(this.VuosinaLB);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dateTimePicker1);
             this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
@@ -140,12 +147,12 @@
 
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label VuosinaLB;
+        private System.Windows.Forms.Label KuukausinaLB;
+        private System.Windows.Forms.Label PaivinaLB;
+        private System.Windows.Forms.Label TunteinaLB;
+        private System.Windows.Forms.Label MinuutteinaLB;
+        private System.Windows.Forms.Label SekunteinaLB;
     }
 }
 
