df9c3ea86035f230d97cf5bc7d68a0dcf526867e
Julius-687 <julius.ikavalko@gmail.com>
2021-10-12 16:01:09 +0300

    BMI-laskuri
    KESKENERÄINEN

---
+++ BMI-laskuri/BMI-laskuri/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMI_laskuri
+{
+    public partial class BMIForm : Form
+    {
+        public BMIForm()
+        {
+            InitializeComponent();
+        }
+
+        private void VastausLB_Click(object sender, EventArgs e)
+        {
+            double paino = 0, pituus = 0, painoindeksi;
+            paino = Convert.ToDouble(PainoTB.Text);
+            pituus = Convert.ToDouble(PituusTB.Text);
+            painoindeksi = Math.Round(paino / (pituus * pituus), 2);
+            if(painoindeksi < 18.5)
+            {
+
+            }
+        }
+    }
+}
